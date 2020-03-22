29637235f53634f0b2b1340c2bf7782e536038fb
Ihor Kovalyk <777kovalik@gmail.com>
2020-03-23 00:00:19 +0200

    Provided the task demand

--- src/main/java/com/ua/task3/Solution.java
+++ src/main/java/com/ua/task3/Solution.java
@@ -1,6 +1,36 @@
 package com.ua.task3;
 
 import java.util.Scanner;
+/**
+ * We define the following terms:
+ *
+ * Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows:
+ * For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+ *
+ * A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+ * Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+ *
+ * Input Format
+ *
+ * The first line contains a string denoting .
+ * The second line contains an integer denoting .
+ *
+ * Constraints
+ *
+ *  consists of English alphabetic letters only (i.e., [a-zA-Z]).
+ * Output Format
+ *
+ * Return the respective lexicographically smallest and largest substrings as a single newline-separated string.
+ *
+ * Sample Input 0
+ *
+ * welcometojava
+ * 3
+ * Sample Output 0
+ *
+ * ava
+ * wel
+ * */
 
 public class Solution {
     public static String getSmallestAndLargest(String s, int k) {
